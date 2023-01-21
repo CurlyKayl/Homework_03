831edca4329af85e1d5056559fff1778e625da1c
Curly_Kayl <endwarfor@gmail.com>
2023-01-21 22:48:37 +0300

    Exercise 23 complete.

--- Exercise_23/Program.cs
+++ Exercise_23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+// Console.WriteLine("Введите число: ");
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Таблица кубов числа {number}: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i*i*i}");
+}
